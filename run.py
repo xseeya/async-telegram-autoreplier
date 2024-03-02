98324682fd9e97859cfb2804a93d2bed09e0264f
Froala <desynq@mail.ru>
2024-03-02 12:58:24 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+
+import datetime
+import time
+
+import config as cfg
+
+app = Client("my_account", api_id=cfg.api_id, api_hash=cfg.api_hash)
+
+@app.on_message(filters.private)
+async def hello(client, message) -> None:
+    async for message in app.get_chat_history(message.chat.id, limit=1, offset=1):
+        info = await app.get_messages(message.chat.id, message.id)
+    
+    if datetime.datetime.now().timestamp() - info.date.timestamp() > cfg.delay:
+        await message.reply(f"{cfg.text}")
+
+
+app.run()
